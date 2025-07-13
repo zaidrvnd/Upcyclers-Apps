c3b9c86fa56ac0769e2ebc8a062be2de504b2419
Zaidan Rivandani <74029884+zaidrvnd@users.noreply.github.com>
2025-07-13 10:04:27 +0700

    Delete FRONTEND directory

--- FRONTEND/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import daStyle from 'eslint-config-dicodingacademy';
-
-export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  daStyle,
-];
--- FRONTEND/src/scripts/views/templates/templates.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable linebreak-style */
-import './component/header-element';
-import './component/footer-element';
